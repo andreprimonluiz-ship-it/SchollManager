bc70e5b089e2b58e007bb78f1b87f60500d0ba3a
Andre Luiz <andreprimonluiz@gmail.com>
2026-05-17 10:54:22 -0300

    StudentsController

---
+++ SchoolManager/Controllers/StudentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManager.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
